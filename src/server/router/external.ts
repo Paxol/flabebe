e060b08e9e28652e6542252e188532172ffad35e
MirkoPassoli <mirko.passoli@gmail.com>
2022-09-09 19:56:49 +0200

    Added latest shortlink query

--- src/server/router/external.ts
+++ src/server/router/external.ts
@@ -4,6 +4,23 @@ import { createShortLink } from "../../utils/createShortLink";
 import { createRouter } from "./context";
 
 export const externalRouter = createRouter()
+	.query("latest", {
+		input: z.object({
+			api_key: z.string(),
+			n: z.number().default(3)
+		}),
+
+		async resolve({ input: { api_key, n }, ctx }) {
+			await verifyApiKey(api_key, ctx);
+
+			return await ctx.prisma.shortLink.findMany({
+				orderBy: {
+					createdAt: "desc"
+				},
+				take: n
+			})
+		}
+	})
 	.mutation("create-link", {
 		input: z.object({
 			api_key: z.string(),
@@ -11,22 +28,28 @@ export const externalRouter = createRouter()
 		}),
 
 		async resolve({ input: { api_key, url }, ctx }) {
-			const verification = await ctx.prisma.verificationToken.findFirst({
-				where: {
-					token: api_key
-				}
-			});
-
-			if (!verification) throw new TRPCError({
-				code: 'UNAUTHORIZED',
-				message: 'API key not found'
-			})
-
-			if (verification.expires.getTime() < new Date().getTime()) throw new TRPCError({
-				code: 'UNAUTHORIZED',
-				message: 'API key expired'
-			})
+			await verifyApiKey(api_key, ctx);
 
 			return await createShortLink({ url, expiresAt: null, slug: null }, ctx.prisma)
 		}
 	})
+
+async function verifyApiKey(api_key: string, ctx: { prisma: any; }) {
+	const verification = await ctx.prisma.verificationToken.findFirst({
+		where: {
+			token: api_key
+		}
+	});
+
+	if (!verification)
+		throw new TRPCError({
+			code: 'UNAUTHORIZED',
+			message: 'API key not found'
+		});
+
+	if (verification.expires.getTime() < new Date().getTime())
+		throw new TRPCError({
+			code: 'UNAUTHORIZED',
+			message: 'API key expired'
+		});
+}
